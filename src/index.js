7d1393209544491a3b885cdd883ed73cd6381f3c
Tinatin <omurovatinatin@gmail.com>
2022-02-16 09:56:20 +0600

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    n = n + '';
+    let reversedText = n.split('').reverse().join('');
+    let reversedNumber = parseInt(reversedText, 10);
+    console.log("reversed number: ", reversedNumber);
+    return reversedNumber;
 }
